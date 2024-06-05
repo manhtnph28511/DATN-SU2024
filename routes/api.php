ac39fd529abf922eeb4eb9a9ceed1604e105d4ea
_Phi <buingocphinn@gmail.com>
2024-06-05 22:31:55 +0700

    feat: route api blog

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Blog\BlogController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Blog
+
+Route::apiResource('blogs',BlogController::class);
